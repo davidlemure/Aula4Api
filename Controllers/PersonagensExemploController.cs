2a128ce955eb71b99d83d602e837de44d43435dc
davidlemure <davidl3mos@gmail.com>
2023-08-22 22:36:22 -0300

    primeiro commit

---
+++ Controllers/PersonagensExemploController.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using RpgApi.Models;
+using RpgApi.Models.Enuns;
+
+namespace RpgApi.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+
+    public class PersonagensExemploController : ControllerBase
+    {
+      private static List<Personagem> personagens = new List<Personagem>()
+        {
+            new Personagem() { Id = 1, Nome = "Frodo", PontosVida=100, Forca=17, Defesa=23, Inteligencia=33, Classe=ClasseEnum.Cavaleiro},
+            new Personagem() { Id = 2, Nome = "Sam", PontosVida=100, Forca=15, Defesa=25, Inteligencia=30, Classe=ClasseEnum.Cavaleiro},
+            new Personagem() { Id = 3, Nome = "Galadriel", PontosVida=100, Forca=18, Defesa=21, Inteligencia=35, Classe=ClasseEnum.Clerigo},
+            new Personagem() { Id = 4, Nome = "Gandalf", PontosVida=100, Forca=18, Defesa=18, Inteligencia=37, Classe=ClasseEnum.Mago},
+            new Personagem() { Id = 5, Nome = "Hobbit", PontosVida=100, Forca=20, Defesa=17, Inteligencia=31, Classe=ClasseEnum.Cavaleiro},
+            new Personagem() { Id = 6, Nome = "Celeborn", PontosVida=100, Forca=21, Defesa=13, Inteligencia=34, Classe=ClasseEnum.Clerigo},
+            new Personagem() { Id = 7, Nome = "Radagast", PontosVida=100, Forca=25, Defesa=11, Inteligencia=35, Classe=ClasseEnum.Mago}
+            
+        };  
+
+        [HttpGet("Get")]
+        public IActionResult GetFirst()
+        {
+            Personagem p = personagens[0];
+            return Ok(p);
+        }
+
+        [HttpGet("GetAll")]
+        public IActionResult Get()
+        {
+            return Ok(personagens);
+        }
+
+        [HttpGet("{id}")]
+
+        public IActionResult GetSingle(int id)
+        {
+            return Ok(personagens.FirstOrDefault(pe => pe.Id == id));
+        }
+
+
+
+
+
+
+
+
+
+
+
+        
+    }
+}
---
+++ Models/Personagem.cs
@@ -0,0 +1,21 @@
+using RpgApi.Models.Enuns;
+
+namespace RpgApi.Models
+{
+    public class Personagem
+    {
+        public int Id { get; set; }
+
+        public string Nome { get; set; } = string.Empty;
+
+        public int PontosVida { get; set; }
+
+        public int Forca { get; set; }
+
+        public int Defesa { get; set; }
+
+        public int Inteligencia { get; set; }
+
+        public ClasseEnum Classe { get; set;}
+    }
+}
